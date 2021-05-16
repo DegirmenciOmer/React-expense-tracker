9970ac22b2ec8c8b36325b7aaf13216f87a694e7
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-05-16 18:54:54 +0200

    add components and context

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <h1>Expense Tracker</h1>
+      <a href='https://youtu.be/XuFDcZABiDQ?list=PLillGF-RfqbY3c2r0htQyVbDJJoBFE6Rb&t=1883'>
+        last time
+      </a>
+    </>
+  )
+}
