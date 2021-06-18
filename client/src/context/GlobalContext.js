bd74742884c379c08de489b066e9ec760cbb9e29
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-06-18 17:09:29 +0200

    deploy

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -2,12 +2,7 @@ import React, { createContext, useReducer } from 'react'
 import AppReducer from './AppReducer'
 
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: 20 },
-    { id: 2, text: 'Salary', amount: 200 },
-    { id: 3, text: 'Honey', amount: -20 },
-    { id: 4, text: 'Keyboard', amount: -120 },
-  ],
+  transactions: [],
 }
 //Create Context
 export const GlobalContext = createContext(initialState)
