15a2e3e3a72bb4e527673c83e5266f0f2304cf35
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-06-18 15:59:31 +0200

    commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,6 @@ export default function Header() {
   return (
     <>
       <h1>Expense Tracker</h1>
-      <a href='https://youtu.be/XuFDcZABiDQ?list=PLillGF-RfqbY3c2r0htQyVbDJJoBFE6Rb&t=1883'>
-        last time
-      </a>
     </>
   )
 }
