6e35b62ea20569574ac2e6a007142c14575f173a
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-07-09 12:56:36 +0200

    add radio buttons for income or expense option

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -6,6 +6,7 @@ export default function AddTransaction() {
 
   const [text, setText] = useState('')
   const [amount, setAmount] = useState('')
+  const [isExpense, setIsExpense] = useState('Income')
   const onSubmit = (e) => {
     e.preventDefault()
     const randomNumber = Math.floor(Math.random() * 10000000000000)
@@ -18,18 +19,40 @@ export default function AddTransaction() {
     addTransaction(newTransaction)
     setAmount('')
     setText('')
+
+    //-Math.abs()
   }
   return (
     <>
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
+        <p>
+          <input
+            type='radio'
+            checked={isExpense === 'Income'}
+            value='Income'
+            onChange={(e) => setIsExpense(e.target.value)}
+          />
+          <label className='radio-input'>Income</label>
+          <input
+            type='radio'
+            checked={isExpense === 'Expense'}
+            value='Expense'
+            onChange={(e) => setIsExpense(e.target.value)}
+          />
+          <label className='radio-input'>Expense</label>
+        </p>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
           <input
             type='text'
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder='Enter text...'
+            placeholder={
+              isExpense === 'Income'
+                ? 'Enter Income Details...'
+                : 'Enter Expense Details...'
+            }
             required
           />
         </div>
@@ -40,8 +63,18 @@ export default function AddTransaction() {
           <input
             type='number'
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            placeholder='Enter amount...'
+            onChange={(e) =>
+              setAmount(
+                isExpense === 'Income'
+                  ? e.target.value
+                  : -Math.abs(e.target.value)
+              )
+            }
+            placeholder={
+              isExpense === 'Income'
+                ? 'Enter Income Amount...'
+                : 'Enter Expense Amount...'
+            }
             required
           />
         </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <>
-      <h1>Expense Tracker</h1>
+      <h1>Income & Expense Tracker</h1>
     </>
   )
 }
