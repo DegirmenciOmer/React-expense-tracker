5ed55cf216f6fb326557211e49495bf00fa5d173
Omer Degirmenci <omer.degirmenci@system4.nl>
2022-05-29 17:33:49 +0200

    fix expense bug, improve code by renaming some  variables

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,85 +1,79 @@
-import React, { useContext, useState } from 'react'
-import { GlobalContext } from '../context/GlobalContext'
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 export default function AddTransaction() {
-  const { addTransaction } = useContext(GlobalContext)
+  const { addTransaction } = useContext(GlobalContext);
 
-  const [text, setText] = useState('')
-  const [amount, setAmount] = useState('')
-  const [isExpense, setIsExpense] = useState('Income')
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [transactionType, setTransactionType] = useState("Income");
   const onSubmit = (e) => {
-    e.preventDefault()
-    const randomNumber = Math.floor(Math.random() * 10000000000000)
+    e.preventDefault();
+    const randomNumber = Math.floor(Math.random() * 10000000000000);
 
     const newTransaction = {
       id: randomNumber,
-      text,
+      description,
       amount: parseInt(amount),
-    }
-    addTransaction(newTransaction)
-    setAmount('')
-    setText('')
-
-    //-Math.abs()
-  }
+    };
+    addTransaction(newTransaction);
+    setAmount("");
+    setDescription("");
+  };
   return (
     <>
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <p>
           <input
-            type='radio'
-            checked={isExpense === 'Income'}
-            value='Income'
-            onChange={(e) => setIsExpense(e.target.value)}
+            type="radio"
+            checked={transactionType === "Income"}
+            value="Income"
+            onChange={(e) => setTransactionType(e.target.value)}
           />
-          <label className='radio-input'>Income</label>
+          <label className="radio-input">Income</label>
           <input
-            type='radio'
-            checked={isExpense === 'Expense'}
-            value='Expense'
-            onChange={(e) => setIsExpense(e.target.value)}
+            type="radio"
+            checked={transactionType === "Expense"}
+            value="Expense"
+            onChange={(e) => setTransactionType(e.target.value)}
           />
-          <label className='radio-input'>Expense</label>
+          <label className="radio-input">Expense</label>
         </p>
-        <div className='form-control'>
-          <label htmlFor='text'>Text</label>
+        <div className="form-control">
+          <label htmlFor="text">Description</label>
           <input
-            type='text'
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder={
-              isExpense === 'Income'
-                ? 'Enter Income Details...'
-                : 'Enter Expense Details...'
-            }
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder={`${
+              transactionType === "Income" ? "Income" : "Expense"
+            } Details...`}
             required
           />
         </div>
-        <div className='form-control'>
-          <label htmlFor='amount'>
+        <div className="form-control">
+          <label htmlFor="amount">
             Amount <br />
           </label>
           <input
-            type='number'
+            type="number"
             value={amount}
             onChange={(e) =>
               setAmount(
-                isExpense === 'Income'
+                transactionType === "Income"
                   ? e.target.value
                   : -Math.abs(e.target.value)
               )
             }
-            placeholder={
-              isExpense === 'Income'
-                ? 'Enter Income Amount...'
-                : 'Enter Expense Amount...'
-            }
+            placeholder={`${
+              transactionType === "Income" ? "Income" : "Expense"
+            } Amount...`}
             required
           />
         </div>
-        <button className='btn'>Add transaction</button>
+        <button className="btn">Add transaction</button>
       </form>
     </>
-  )
+  );
 }
--- client/src/components/IncomeExpense.js
+++ client/src/components/IncomeExpense.js
@@ -5,7 +5,7 @@ import { GlobalContext } from '../context/GlobalContext'
 export default function IncomeExpense() {
   const { transactions } = useContext(GlobalContext)
 const amounts  = transactions.map(transaction=>transaction.amount)
-const expenses = amounts.filter(amount=>amount<0).reduce((acc, amount)=> (acc+=amount),0).toFixed(2)
+const expenses = amounts.filter(amount=>amount<0).reduce((acc, amount)=> (acc-=amount),0).toFixed(2)
 const income = amounts.filter(amount=>amount>0).reduce((acc, amount)=> (acc+=amount),0).toFixed(2)
 
 
